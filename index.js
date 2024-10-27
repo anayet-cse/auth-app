f05a4975aa0764c177956a8d2958fa2a80d6bcfc
Anayet <anayet.cse@gmail.com>
2024-10-27 17:17:40 +0600

    Implement basic user registration without validation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const util = require('util');
 const express = require('express');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+
 const db = require('./src/config/db');
+const ApiResponseMessage = require('./utils/utils')
+
+const port = process.env.PORT || 3000;
+const query = util.promisify(db.query).bind(db);
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
 db.connect((err) => {
   if (err) {
@@ -12,4 +17,32 @@ db.connect((err) => {
     return;
   }
   console.log(`connected to mysql db ${port}`);
+});
+
+app.listen(port, ()=> {
+    console.log(`app is running at ${port}`);
+});
+
+app.post('/users', async function(req, res) {
+  try {
+    const {firstName, lastName, nid,
+        profilePhoto, age, maritalStatus, email, password
+    } = req.body;
+
+    await query(
+        'INSERT INTO users (firstName, lastName, nid, profilePhoto, age, maritalStatus) VALUES (?, ?, ?, ?, ?, ?)', 
+        [firstName, lastName, nid, profilePhoto, age, maritalStatus]
+    );
+    await query(
+        'INSERT INTO auth (email, password) VALUES (?, ?)', 
+        [email, password]
+    );
+    res.status(201).send({
+        "message": ApiResponseMessage.USER_CREATE
+    });
+  } catch (error) {
+    res.status(400).send({
+        "message": ApiResponseMessage.SYSTEM_ERROR
+    });
+  }
 });
